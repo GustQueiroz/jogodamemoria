9765de9d8bc46c46571f0df67e3fc2ab1f6c8f8f
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2023-11-28 21:22:30 -0300

    mudança musica

--- scripts/interface.js
+++ scripts/interface.js
@@ -3,165 +3,158 @@ const $back = "card_back";
 const $card = "card";
 const $icon = "icon";
 
-const startGameBtn = document.getElementById('startGame');
-
-
+const startGameBtn = document.getElementById("startGame");
 
 initializeCards(game.createCards());
 
 let clock;
 
-
 function startGame() {
-    initializeCards(game.createCards());
-    setTimeout(() => document.querySelector('.btnStart').classList.add('btn_flip'), 2000)
-    preview()
-    setTimeout(() => startTimer(),3000)
-    setTimeout(() => game.lockMode = false, 3500)
-    startGameBtn.disabled = true;
+  initializeCards(game.createCards());
+  setTimeout(
+    () => document.querySelector(".btnStart").classList.add("btn_flip"),
+    2000
+  );
+  preview();
+  audio.play();
+  setTimeout(() => startTimer(), 3000);
+  setTimeout(() => (game.lockMode = false), 3500);
+  startGameBtn.disabled = true;
 }
 
 function initializeCards(cards) {
-    let gameBoard = document.getElementById('gameBoard');
-    gameBoard.innerHTML = '';
-    // criar as cartas
-    game.cards.forEach((card => {
+  let gameBoard = document.getElementById("gameBoard");
+  gameBoard.innerHTML = "";
+  // criar as cartas
+  game.cards.forEach((card) => {
+    let cardElement = document.createElement("div");
+    cardElement.id = card.id;
+    cardElement.classList.add($card);
+    cardElement.dataset.icon = card.icon;
 
-        let cardElement = document.createElement('div');
-        cardElement.id = card.id;
-        cardElement.classList.add($card);
-        cardElement.dataset.icon = card.icon;
-        
-        createCardContent(card, cardElement);
-        
-        cardElement.addEventListener('click', flipCard);
-        gameBoard.appendChild(cardElement)
-    }))
+    createCardContent(card, cardElement);
+
+    cardElement.addEventListener("click", flipCard);
+    gameBoard.appendChild(cardElement);
+  });
 }
 
 function preview() {
-    let cards = document.querySelectorAll('.card')
-    cards.forEach(card => {
-        card.classList.add('flip')
-        setTimeout(() => {
-            card.classList.remove('flip');
-        }, 3000)
-    })
+  let cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.classList.add("flip");
+    setTimeout(() => {
+      card.classList.remove("flip");
+    }, 3000);
+  });
 }
 
-
 function createCardContent(card, cardElement) {
-
-    createCardFace($front, card, cardElement);
-    createCardFace($back, card, cardElement);
-
-
+  createCardFace($front, card, cardElement);
+  createCardFace($back, card, cardElement);
 }
 
 function createCardFace(face, card, element) {
-
-    let cardElementFace = document.createElement('div')
-    cardElementFace.classList.add(face)
-    if (face == $front) {
-        let iconElement = document.createElement('img')
-        iconElement.classList.add($icon);
-        iconElement.src = `./assets/images/${card.icon}.svg`
-        iconElement.alt = `logo do ${card.icon}`
-        cardElementFace.appendChild(iconElement)
-    } else {
-        cardElementFace.innerHTML = `<img src="./assets/images/logo.png" alt="logo dos vingadores">`
-    }
-    element.appendChild(cardElementFace);
+  let cardElementFace = document.createElement("div");
+  cardElementFace.classList.add(face);
+  if (face == $front) {
+    let iconElement = document.createElement("img");
+    iconElement.classList.add($icon);
+    iconElement.src = `./assets/images/${card.icon}.svg`;
+    iconElement.alt = `logo do ${card.icon}`;
+    cardElementFace.appendChild(iconElement);
+  } else {
+    cardElementFace.innerHTML = `<img src="./assets/images/logo.png" alt="logo dos vingadores">`;
+  }
+  element.appendChild(cardElementFace);
 }
 
-
 function flipCard() {
-    if (game.setCard(this.id)) {
-        this.classList.add('flip');
-        if (game.secondCard) {
-            if (game.checkMatch()) {
-                game.clearCards();
-                hitSound.play()
-                console.log('acertou');
-                if (game.checkGameOver()) {
-                    stopTimer();
-                    showLayers();
-                }
-            } else {
-                // desvira as cartas
-                console.log('errou');
-                setTimeout(function () {
-                    missSound.play();
-
-                    let firstCardView = document.getElementById(game.firstCard.id);
-                    let secondCardView = document.getElementById(game.secondCard.id);
-
-                    firstCardView.classList.remove('flip');
-                    secondCardView.classList.remove('flip');
-                    game.unflipCards();
-                }, 1000)
-            }
+  if (game.setCard(this.id)) {
+    this.classList.add("flip");
+    if (game.secondCard) {
+      if (game.checkMatch()) {
+        game.clearCards();
+        hitSound.play();
+        console.log("acertou");
+        if (game.checkGameOver()) {
+          stopTimer();
+          showLayers();
         }
+      } else {
+        // desvira as cartas
+        console.log("errou");
+        setTimeout(function () {
+          missSound.play();
+
+          let firstCardView = document.getElementById(game.firstCard.id);
+          let secondCardView = document.getElementById(game.secondCard.id);
+
+          firstCardView.classList.remove("flip");
+          secondCardView.classList.remove("flip");
+          game.unflipCards();
+        }, 1000);
+      }
     }
+  }
 }
 
 function showLayers() {
-    let gameOverLayer = document.getElementById('gameOver');
-    let yayLayer = document.getElementById('yay');
-    let result = document.getElementById('result');
+  let gameOverLayer = document.getElementById("gameOver");
+  let yayLayer = document.getElementById("yay");
+  let result = document.getElementById("result");
 
-    yayLayer.style.display = 'flex';
-    setTimeout(() => {
-        yayLayer.style.display = 'none';
-        result.innerText += timeResult;
-        gameOverLayer.style.display = 'flex';
-    }, 3000)
+  yayLayer.style.display = "flex";
+  setTimeout(() => {
+    yayLayer.style.display = "none";
+    result.innerText += timeResult;
+    gameOverLayer.style.display = "flex";
+  }, 3000);
 }
 
 function restart() {
-    location.reload();
+  location.reload();
 }
 
 function gg() {
-    game.flipAll()
-    let avengers = document.querySelectorAll('.card');
-    document.querySelectorAll('.card').forEach((avenger) => {
-        avenger.classList.add('flip');
-        avenger.classList.add('gradient-border');
-    })
-    showLayers()
+  game.flipAll();
+  let avengers = document.querySelectorAll(".card");
+  document.querySelectorAll(".card").forEach((avenger) => {
+    avenger.classList.add("flip");
+    avenger.classList.add("gradient-border");
+  });
+  showLayers();
 }
 
-
-const clockDisplay = document.getElementById('clock');
+const clockDisplay = document.getElementById("clock");
 
 let minutes = 0;
 let aux = 0;
-let seconds = 0
+let seconds = 0;
 let timeResult;
 
 function timer() {
-    if (seconds < 60) {
-        aux += 1;
-        seconds = aux < 10 ? '0' + aux : aux
-        clockDisplay.innerHTML = `0${minutes}:${seconds}`;
-    } else {
-        aux = 0;
-        seconds = 0;
-        minutes += 1;
-    }
+  if (seconds < 60) {
+    aux += 1;
+    seconds = aux < 10 ? "0" + aux : aux;
+    clockDisplay.innerHTML = `0${minutes}:${seconds}`;
+  } else {
+    aux = 0;
+    seconds = 0;
+    minutes += 1;
+  }
 }
 
-function startTimer(){
-    clock = setInterval(timer, 1000);
+function startTimer() {
+  clock = setInterval(timer, 1000);
 }
 
 function stopTimer() {
-    clearInterval(clock);
-    if (minutes < 1) {
-        timeResult = `${seconds} segundos`;
-    } else {
-        timeResult = `0${minutes}:${seconds}`;
-    }
-    console.log('Tempo foi ' + timeResult);
+  clearInterval(clock);
+  if (minutes < 1) {
+    timeResult = `${seconds} segundos`;
+  } else {
+    timeResult = `0${minutes}:${seconds}`;
+  }
+  console.log("Tempo foi " + timeResult);
 }
--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,21 +1,28 @@
 // sound effects
-const missSound = document.getElementById('missSound');
-const hitSound = document.getElementById('hitSound');
-const muteBtn = document.querySelector('#muteBtn');
-const audios = document.querySelectorAll('audio');
+const missSound = document.getElementById("missSound");
+const hitSound = document.getElementById("hitSound");
+const muteBtn = document.querySelector("#muteBtn");
+const audios = document.querySelectorAll("audio");
 
 function muteSounds() {
-    audios.forEach(audio => {
-        if (audio.muted == false) {
-            audio.muted = true;
-        } else {
-            audio.muted = false;
-        }
-    })
-    changeBg(audios[0].muted)
+  audios.forEach((audio) => {
+    if (audio.muted == false) {
+      audio.muted = true;
+    } else {
+      audio.muted = false;
+    }
+  });
+  changeBg(audios[0].muted);
 }
 
 function changeBg(boolean) {
-    boolean ? muteBtn.style.backgroundImage = 'url(./assets/images/audio/muted.svg)' : muteBtn.style.backgroundImage = 'url(./assets/images/audio/full.svg)'
+  boolean
+    ? (muteBtn.style.backgroundImage = "url(./assets/images/audio/muted.svg)")
+    : (muteBtn.style.backgroundImage = "url(./assets/images/audio/full.svg)");
 }
 
+var audio = document.getElementById("myAudio");
+
+document.addEventListener("DOMContentLoaded", function () {
+  audio.volume = 0.1;
+});
